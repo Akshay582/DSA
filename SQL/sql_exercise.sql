f15489a506b05a6316624952fbd54d3d27963e6f
akshay.mittal <akshay.mittal@avalara.com>
2024-03-03 12:48:57 +0530

    add sql

--- SQL/sql_exercise.sql
+++ SQL/sql_exercise.sql
@@ -27,10 +27,42 @@
 SELECT  first_name
        ,last_name
        ,city
-       ,[state]
+       ,state
 FROM salespersons sp
-INNER JOIN [address] ad
+INNER JOIN address ad
 ON sp.salesperson_id = ad.salesperson_id
+
+-- Output:
+
+-- +------------+-----------+-------------+------------+
+-- | first_name | last_name | city        | state      |
+-- +------------+-----------+-------------+------------+
+-- | Jones      | Collins   | Los Angeles | California |
+-- | Bryant     | Davis     | Denver      | Colorado   |
+-- +------------+-----------+-------------+------------+
+
+-- The above doesn't work as the inner join removes the 
+-- salesperson which does not have the salesperson_id
+-- in the address table
+
+SELECT  first_name
+       ,last_name
+       ,city
+       ,state
+FROM salespersons
+LEFT JOIN address
+ON salespersons.salesperson_id = address.salesperson_id;
+
+-- Output:
+
+-- +------------+-----------+-------------+------------+
+-- | first_name | last_name | city        | state      |
+-- +------------+-----------+-------------+------------+
+-- | Green      | Wright    | NULL        | NULL       |
+-- | Jones      | Collins   | Los Angeles | California |
+-- | Bryant     | Davis     | Denver      | Colorado   |
+-- +------------+-----------+-------------+------------+
+
 
 -- 2. From the following table, write a SQL query to find the third highest sale. Return sale amount.
 -- Input:
@@ -48,4 +80,52 @@ ON sp.salesperson_id = ad.salesperson_id
 -- SecondHighestSale|
 -- -----------------|
 --              4500|
+
+SELECT MIN(sale_amt) as ThirdHighestSale
+FROM salemast
+GROUP BY sale_amt
+ORDER BY sale_amt DESC
+LIMIT 3
+
+-- Output:
+
+-- +------------------+
+-- | ThirdHighestSale |
+-- +------------------+
+-- |             5500 |
+-- |             4500 |
+-- |             3500 |
+-- +------------------+
+
+-- The above doesn't work as I am applying aggregate on 
+-- a column that already has a group by
+
+SELECT  DISTINCT sale_amt AS ThirdHighestSale
+FROM salemast
+ORDER BY sale_amt DESC
+LIMIT 1 OFFSET 2;
+
+-- Output:
+
+-- +------------------+
+-- | ThirdHighestSale |
+-- +------------------+
+-- |             3500 |
+-- +------------------+
+
+-- 3. From the following table, write a SQL query to find the Nth highest sale. Return sale amount.
+-- Input:
+
+-- table: salemast
+
+-- sale_id|employee_id|sale_date |sale_amt|
+-- -------|-----------|----------|--------|
+--       1|       1000|2012-03-08|    4500|
+--       2|       1001|2012-03-09|    5500|
+--       3|       1003|2012-04-10|    3500|
+-- Output:
+
+-- getNthHighestSaleAmt(3)|
+-- -----------------------|
+--                    3500|
 
